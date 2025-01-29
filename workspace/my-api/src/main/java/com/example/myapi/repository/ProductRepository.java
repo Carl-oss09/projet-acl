8ad3eff55afe0e863c275b337f8a7adf239c7874
Carla Grimaud <307804@etud.uco.fr>
2025-01-29 09:51:36 +0100

    Suppression de product dans tout le projet

--- workspace/my-api/src/main/java/com/example/myapi/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapi.repository;
-
-import com.example.myapi.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
