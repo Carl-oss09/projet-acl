8ff9430c52daedbae9b307af813111ae53eea96c
Carla Grimaud <307804@etud.uco.fr>
2025-01-29 12:16:39 +0100

    Ajout de aprem matin dans recherche + places restantes

--- workspace/mywebapp/src/main/java/com/example/mywebapp/MywebappApplication.java
+++ workspace/mywebapp/src/main/java/com/example/mywebapp/MywebappApplication.java
@@ -1,5 +1,8 @@
 package com.example.mywebapp;
 
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.http.HttpRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,8 +13,22 @@ public class MywebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MywebappApplication.class, args);
-		System.out.println("Ouvrez cette URL dans votre navigateur : http://localhost:8082/login");
 
+		// Ouvrir l'URL dans le navigateur après le démarrage
+		openBrowser("http://localhost:8082/login");
 	}
 
+	private static void openBrowser(String url) {
+		try {
+			// Vérifier si l'OS supporte l'ouverture dans un navigateur
+			if (Desktop.isDesktopSupported()) {
+				Desktop.getDesktop().browse(URI.create(url));
+			} else {
+				System.out.println("Impossible d'ouvrir le navigateur par défaut");
+				System.out.println("Ouvrez le lien suivant : http://localhost:8082/login");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
